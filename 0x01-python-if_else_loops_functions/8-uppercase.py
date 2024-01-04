69f087080d5b968bf1e6ec93c0e3e82704ba5e94
amateurManu <oderoemmanuel6@gmail.com>
2024-01-03 18:46:36 -0800

    corrected code to print out in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for a in range(len(str)):
-        if ord(str[a]) >= 97 and ord(str[a] <= 122:
-                number = 32
+        if ord(str[a]) >= 97 and ord(str[a]) <= 122:
+            number = 32
         else:
             number = 0
         print("{:c}".format(ord(str[a]) - number), end='')

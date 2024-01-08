d62bff1ceece141b09c3aa5e80983e9e515b5b12
amateurManu <oderoemmanuel6@gmail.com>
2024-01-08 15:56:24 -0800

    corrected equality sign

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,8 +3,10 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
 
+    #first = my_list[0]
+
     for biggest in range(1, len(my_list)):
-        if my_list[biggest] = my_list[0]:
+        if my_list[biggest] > my_list[0]:
             my_list[0] = my_list[biggest]
 
     return (my_list[0])

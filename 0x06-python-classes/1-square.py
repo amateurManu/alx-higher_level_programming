767ff63d76d589c90fa32a4cf90b6b1e1b563ac3
amateurManu <oderoemmanuel6@gmail.com>
2024-01-23 18:29:44 -0800

    initialized method

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module defining a square based on 0-square.py
+"""
+class Square:
+    """Class Square that deffines a square object
+    """
+    def __init__(self, size):
+        """Initializes a method that stores size of square
+
+        Args:
+            param1 (int): size of square
+        """
+        self.__size = size

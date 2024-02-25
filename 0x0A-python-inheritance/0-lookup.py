d0f84b1a3ca93ec11629f4eb70d798de7a7dda89
amateurManu <oderoemmanuel6@gmail.com>
2024-02-25 11:34:30 -0800

    corrected code to return available attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """ Function that returns list of available attributes and
-        methods of an object
+    """ Function that returns the list of available attributes
+        and methods of an object
 
     Args:
-        obj: class instance
+        obj: instance of the class
 
     Returns:
         List of attributes

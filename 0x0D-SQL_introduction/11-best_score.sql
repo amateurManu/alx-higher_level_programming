f578b447c02494e93658b30fe5b23acf3e50fe5c
amateurManu <oderoemmanuel6@gmail.com>
2024-04-06 08:08:42 -0700

    added script that lists records meeting criteria

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Script that lists records with a score greater than 9
+-- Query to list records meeting a criterion in the table
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

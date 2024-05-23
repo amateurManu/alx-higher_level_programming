991f07b6033afcda46629ac17969ca4dbc8302c1
amateurManu <oderoemmanuel6@gmail.com>
2024-05-23 13:16:55 -0700

    edited script to clear semistandard errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (err, content) {
-	if (content === undefined) {
-		console.log(err);
-	} else {
-		console.log(content);
-	}
+  if (content === undefined) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });

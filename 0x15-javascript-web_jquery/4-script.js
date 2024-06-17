1b799704170f74d8019da08c3964029420766006
Emmanuel Odero <oderoemmanuel6@gmail.com>
2024-06-17 17:05:26 +0300

    edited file
    edited file to be semistandard compliant

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,10 +1,10 @@
 const $ = window.$;
 $('DIV#toggle_header').click(function () {
-	if ($('HEADER').hasClass('red')) {
-		$('HEADER').removeClass('red');
-		$('HEADER').addClass('green');
-	} else {
-		$('HEADER').removeClass('green');
-		$('HEADER').addClass('red');
-	}
+  if ($('HEADER').hasClass('red')) {
+    $('HEADER').removeClass('red');
+    $('HEADER').addClass('green');
+  } else {
+    $('HEADER').removeClass('green');
+    $('HEADER').addClass('red');
+  }
 });
--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-const $ = window.$
+const $ = window.$;
 $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data, textStatus) {
-	$('DIV#character').text(data.name);
+  $('DIV#character').text(data.name);
 });
